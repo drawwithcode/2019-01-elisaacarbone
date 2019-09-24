cdadb2e23e607153fa2fc96299dbb8b7428173bb
Elisa Carbone <carbone.elisa.98.ec@gmail.com>
2019-09-24 08:56:05 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,50 @@
+
+
 function preload(){
   // put preload code here
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(30);
+  angleMode(DEGREES);
+
+  frameRate(60);
 }
 
 function draw() {
-  // put drawing code here
+
+  translate(width/2,height/2)
+  stroke(
+    lerpColor(
+      color (249, 57, 67),
+      color (126, 178, 221),
+      frameCount/480
+    )
+  );
+
+  if (frameCount <=120) {
+    push();
+    translate(-100,0);
+    line(100, 0, 100*sin(frameCount*3 + 90), 100*cos(frameCount*3 + 90));
+    pop();
+  }
+  else {if (frameCount <= 240){
+      push();
+      translate(0,100);
+      line(0, -100, 100*sin(frameCount*3 + 180), 100*cos(frameCount*3 + 180));
+      pop();}
+      else {if(frameCount <= 360){
+            translate(100,0);
+            line(-100, 0, 100*sin(frameCount*3 - 90), 100*cos(frameCount*3 - 90));
+          }
+        else {
+          translate(0, -100);
+          line(0, 100, 100*sin(frameCount*3), 100*cos(frameCount*3));}
+        }
+  }
+
+  if (frameCount >= 480){
+    background(30);
+    frameCount = -1;}
 }
